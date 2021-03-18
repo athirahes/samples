7ff5e15e8240e7c490763e458d75b157c64b3e73
Shafiqah Nur Athirah Binti Hassan <shafiqahnurathirah@gmail.com>
2021-03-18 21:54:08 +0800

    Fourth Commit - Exercise 5

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -2,6 +2,8 @@ package samples.demo;
 
 public class Location {
 	private String name;
+	private int locationId;
+	private int description;
 
 	public void setName(String name) {
 		this.name = name;
@@ -9,5 +11,21 @@ public class Location {
 	
 	public String getName() {
 		return name;
+	}
+
+	public int getLocationId() {
+		return locationId;
+	}
+
+	public void setLocationId(int locationId) {
+		this.locationId = locationId;
+	}
+
+	public int getDescription() {
+		return description;
+	}
+
+	public void setDescription(int description) {
+		this.description = description;
 	}
 }
